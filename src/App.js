8f01be9a8413ac89c303d9a6a9067d6a222f5737
ewalezny <ewa.lezny@gmail.com>
2022-02-24 13:29:22 +0100

    statistics section done

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import "./scss/main.scss";
 import Header from "./components/Header";
 import Opening from "./components/Opening";
+import Statistics from "./components/Statistics";
 
 const App = () => {
     return (
         <>
             <Header/>
             <Opening/>
+            <Statistics/>
         </>
     )
 }
---
+++ src/components/Statistics/Statistics.js
@@ -0,0 +1,35 @@
+import React from "react";
+import styles from "./statistics.module.scss";
+import StatisticsBox from "./StatisticsBox";
+import Circle from "./Circle";
+
+const Statistics = () => {
+    const {statistics, container, statistics__content, statistics__box, statistics__img_person, statistics__img_eye, statistics__img_catalog} = styles;
+
+    return (
+        <section className={statistics} id="statistics">
+            <div className={container}>
+                <h2>The most popular sales platform in country</h2>
+                <div className={statistics__content}>
+                    <div className={statistics__box}>
+                        <Circle redish />
+                        <img src="../../../public/assets/Person.svg" alt="person" className={statistics__img_person}/>
+                        <StatisticsBox number="100000+" text="daily entries"/>
+                    </div>
+                    <div className={statistics__box}>
+                        <Circle primary />
+                        <img src="../../../public/assets/Eye.svg" alt="eye" className={statistics__img_eye}/>
+                        <StatisticsBox number="2000000+" text="seen products every day"/>
+                    </div>
+                    <div className={statistics__box}>
+                        <Circle green />
+                        <img src="../../../public/assets/Catalog.svg" alt="catalog" className={statistics__img_catalog}/>
+                        <StatisticsBox number="10000+" text="added posts daily"/>
+                    </div>
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default Statistics;
